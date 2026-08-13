9bee651611a9e96449c6d37504c957562b591f48
welcomedigitalbox-web <welcome.digitalbox@gmail.com>
2026-08-13 16:45:41 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           <AuthProvider>
             <StoreProvider>
               <Nav />
-              <main className="sm:ml-20 max-w-6xl mx-auto px-4 pb-16">{children}</main>
+              <main className="sm:ml-20 w-full px-4 sm:px-6 pb-16">{children}</main>
             </StoreProvider>
           </AuthProvider>
         </LanguageProvider>
